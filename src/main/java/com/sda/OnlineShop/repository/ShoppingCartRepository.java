59c2ebc3f7e60d90c8a567a441d8aaf0b6ff01b1
TudorMateut <tudormateut@yahoo.com>
2023-02-27 21:37:02 +0200

    implemented addToCartService and created checkout page

---
+++ src/main/java/com/sda/OnlineShop/repository/ShoppingCartRepository.java
@@ -0,0 +1,8 @@
+package com.sda.OnlineShop.repository;
+
+import com.sda.OnlineShop.entities.ShoppingCart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Integer> {
+    ShoppingCart findByUserEmailAddress(String emailAddress);
+}
